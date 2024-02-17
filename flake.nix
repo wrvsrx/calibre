8ba85d264d91d24e61822ea3b0bf517124a07bfc
wrvsrx <wrvsrx@outlook.com>
2024-02-17 16:46:49 +0800

    fix env

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } ({ inputs, ... }: {
     systems = [ "x86_64-linux" ];
     perSystem = { pkgs, ... }: rec {
-      packages.default = pkgs.qt6Packages.callPackage ./default.nix { };
+      packages.default = pkgs.qt6Packages.callPackage ./default.nix { podofo = pkgs.podofo010; };
       devShells.default = pkgs.mkShell { inputsFrom = [ packages.default ]; };
       formatter = pkgs.nixpkgs-fmt;
     };
