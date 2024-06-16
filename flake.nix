dfd17a64ae2c0205d558c3be9751f79defdf12e5
wrvsrx <wrvsrx@outlook.com>
2024-06-16 19:32:39 +0800

    chore(env): config dev env

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "flake template";
+
+  inputs = {
+    flake-lock.url = "github:wrvsrx/flake-lock";
+    nixpkgs.follows = "flake-lock/nixpkgs";
+    flake-parts.follows = "flake-lock/flake-parts";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { inputs, ... }:
+      {
+        systems = [ "x86_64-linux" ];
+        perSystem =
+          { pkgs, ... }:
+          {
+            devShells.default = pkgs.mkShell {
+              inputsFrom = [ pkgs.calibre ];
+            };
+            formatter = pkgs.nixfmt-rfc-style;
+          };
+      }
+    );
+}
